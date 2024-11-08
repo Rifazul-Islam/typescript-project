dcf1613cc80a1f78ada906e75c15712cb16da137
Rifazul-Islam <rifazul50@gmail.com>
2024-11-08 16:55:50 +0600

    use left sidebar modal

--- src/Pages/OptimizationMatrix/OptimiztionMatrix.tsx
+++ src/Pages/OptimizationMatrix/OptimiztionMatrix.tsx
@@ -8,9 +8,10 @@ import {
 } from "react-icons/io";
 
 const OptimiztionMatrix = () => {
+  // This A array Object
   const items: { title: string; num: number }[] = [
     { title: "Incident Management", num: 1 },
-    { title: "Alarm Managements", num: 2 },
+    { title: "Managements", num: 2 },
     { title: "System Visualization", num: 3 },
     { title: "Actuator Control", num: 4 },
     { title: "Realtime Analytics", num: 5 },
@@ -42,40 +43,60 @@ const OptimiztionMatrix = () => {
     { title: "Technology Features1", num: 31 },
   ];
 
+  // For Top Side use This Sistem
   const [openIndex, setOpenIndex] = useState<number | null>(null);
   const [openIndexs, setOpenIndexs] = useState<number | null>(null);
 
-  // th use
+  //  Toggle the specific cell when clicked // table head th for use this function
   const handleCellClick = (index: number) => {
-    // Toggle the specific cell when clicked
     setOpenIndex(openIndex === index ? null : index);
   };
 
-  // tr use
+  // tr use roated Number PerPuse This Function
   const handleCellClicks = (index: number) => {
-    // Toggle the specific cell when clicked
     setOpenIndexs(openIndexs === index ? null : index);
   };
-
   const [numberStore, setNumberStore] = useState<number | null>(null);
+
+  // =================== Left Side ================
+
+  const [openIndexLeft, setOpenIndexLeft] = useState<number | null>(null);
+  const [openIndexLefts, setOpenIndexLefts] = useState<number | null>(null);
+
+  // td use for
+  const handleCellClickLeft = (index: number) => {
+    setOpenIndexLeft(openIndexLeft === index ? null : index);
+  };
+
+  // td use for
+  const handleCellClickLefts = (index: number) => {
+    setOpenIndexLefts(openIndexLefts === index ? null : index);
+  };
 
   return (
     <div className="bg-purple-100 relative">
       <div className="flex relative">
-        {/* Top Left to right side Node   */}
-        <div className="bg-purple-800 w-full lg:w-72 h-48  text-center pt-14 flex items-center justify-center">
+        {/*  SIEMENS Top Left to right side  */}
+        <div className="bg-purple-800 w-full lg:w-72 h-48  text-center  flex items-center justify-center">
           <div>
-            <h2 className="leading-none text-gray-500 font-bold">SIEMENS</h2>
-            <h3 className="leading-none text-gray-500 font-bold">cncrgy</h3>
+            <h2 className="leading-none text-gray-400 font-bold text-xl">
+              SIEMENS
+            </h2>
+            <h3 className="leading-none text-gray-500 font-bold text-xl">
+              cncrgy
+            </h3>
           </div>
         </div>
+
+        {/* different th porpuse this div use */}
         <div className="w-52 hidden  lg:flex mt-[77px] items-center pt-[38px] flex-col space-y-0 text-sm -ml-[125px]  h-14 -rotate-90 ">
           <p className="bg-purple-950  text-[13px] z-10 h-8 w-52  p-[14px] flex justify-end items-center text-gray-500">
             Teachnology Features
           </p>
         </div>
 
-        <div className="bg-slate-50 w-full h-56 lg:flex hidden relative -ml-[63px]">
+        {/* Main Table th porpuse this div use */}
+        <div className="bg-slate-50 w-full h-[219px] lg:flex hidden relative -ml-[63px]">
           <table className="border-collapse border border-slate-40 absolute ">
             <thead>
               <tr>
@@ -88,6 +109,7 @@ const OptimiztionMatrix = () => {
                     } relative cursor-pointer h-36 border-b-green-300 border-b-4 vertical-text whitespace-nowrap`}
                     key={index}
                   >
+                    {/* this condion use  specific number qual  */}
                     {openIndexs === item?.num ? (
                       <>
                         <span className="text-[14px] font-normal">
@@ -132,7 +154,7 @@ const OptimiztionMatrix = () => {
                             : ""
                         }  bg-[#fff] rotate-90 -top-[227px] -left-14  border-green-400 shadow-xl -z-50 `}
                       >
-                        <span className="text-[28px] absolute text-red-700  flex items-center justify-end h-full mt-1 -mr-1.5 shadow-2xl">
+                        <span className="text-[28px] absolute text-red-700  flex items-center justify-end h-full mt-0.5 -mr-1 shadow-2xl">
                           <IoIosCloseCircleOutline />
                         </span>
                         <h2 className="pr-8 text-lg font-semibold p-4">
@@ -149,6 +171,7 @@ const OptimiztionMatrix = () => {
             </thead>
 
             <tbody>
+              {/* // Mapping this Number show porpuse */}
               <tr>
                 {items.map((item, index) => (
                   <td
@@ -197,6 +220,7 @@ const OptimiztionMatrix = () => {
                 {}
               </tr>
 
+              {/* Emty table row and column use this array like object */}
               {Array.from({ length: 28 }).map((_, rowIndex) => (
                 <tr key={rowIndex}>
                   {items.map((item, index) => (
@@ -210,17 +234,20 @@ const OptimiztionMatrix = () => {
             </tbody>
           </table>
         </div>
-        <p className="bg-black lg:flex hidden font-bold text-white absolute top-[153px] left-[224px] p-1 ">
+
+        {/* It is Position porpuse use */}
+        <p className="bg-black lg:flex hidden font-bold text-white absolute top-[146px] left-[224px] p-1 ">
           UT
         </p>
       </div>
 
       {/* left sidebar  */}
       <div className="h-full  w-[254px] bg-slate-50 -mt-[39px] hidden lg:flex">
-        <div className="z-0">
+        <div className="z-10">
+          {/* This Table Part */}
           <table className="border-collapse border  border-slate-400">
             <thead>
-              <tr className="bg-purple-900 h-8 ">
+              <tr className="bg-purple-900 h-8">
                 <th className="border border-purple-800 text-gray-400">
                   Initiatives
                 </th>
@@ -235,30 +262,105 @@ const OptimiztionMatrix = () => {
             <tbody>
               {items?.map((item, index) => (
                 <tr key={index}>
-                  <td className="border-[1px]  flex items-center p-0.5 justify-between ">
-                    <p className="flex flex-col">
-                      <span className="leading-none text-[11px] text-gray-700">
-                        Manage Safety
-                      </span>
-                      <span className="leading-none text-sm ">
-                        {item?.title.length < 14 ? (
-                          <>{item?.title}</>
-                        ) : (
-                          <>{item?.title.slice(0, 16) + "..."}</>
-                        )}
-                      </span>
-                    </p>
-
-                    <span className="bg-fuchsia-200  rounded-3xl flex items-center justify-between px-2">
-                      0
-                      <IoIosArrowForward className="text-fuchsia-500 " />1
+                  {/* left side Title show  */}
+                  <td
+                    onClick={() => handleCellClickLefts(item?.num)}
+                    className={`border border-r-4 border-r-yellow-300 border-slate-300  ${
+                      openIndexLefts === item?.num &&
+                      "border-r-4 border-r-yellow-600 bg-yellow-100"
+                    } relative cursor-pointer pt-1 flex justify-between items-center`}
+                    key={index}
+                  >
+                    {/* Left side  title Show */}
+                    <span className="text-[14px] font-normal -mt-2 py-0.5">
+                      <span className=" text-gray-700">Manage Safety</span>
+                      {item?.title.length < 14 ? (
+                        <h2 className="leading-none"> {item?.title}</h2>
+                      ) : (
+                        <h2 className="leading-none">
+                          {item?.title.slice(0, 15) + "..."}
+                        </h2>
+                      )}
                     </span>
+                    {/* this dive right side button Show */}
+                    <span className="bg-fuchsia-200  rounded-3xl flex items-center justify-between px-4 h-7 ">
+                      0
+                      <IoIosArrowForward />1
+                    </span>
+
+                    {/* this is Modal design  */}
+                    {openIndexLefts === item?.num && (
+                      <div
+                        className={`absolute  ${
+                          openIndexLefts === 28 ||
+                          openIndexLefts === 29 ||
+                          openIndexLefts === 30 ||
+                          openIndexLefts === 31
+                            ? "left-20"
+                            : ""
+                        }   border w-60 h-36 -top-0.5 bg-[#fff] left-48 z-20  border-yellow-400 shadow-xl `}
+                      >
+                        <span className="text-[28px] absolute text-red-700 pl-[210px]  h-full  shadow-2xl">
+                          <IoIosCloseCircleOutline />
+                        </span>
+                        <h2 className="pr-8 text-lg font-semibold p-4">
+                          {item.title}
+                        </h2>
+                        <div className="border-r-[0.5px] mr-4 flex justify-between p-4">
+                          <p>Custom</p> <p>Data</p>
+                        </div>
+                      </div>
+                    )}
                   </td>
-                  <td className="border border-slate-300 border-l-4 border-l-yellow-400 text-center font-bold">
+
+                  {/* Number Show porpuse use */}
+                  <td className="border border-slate-300 text-center font-bold">
                     {item?.num}
                   </td>
-                  <td className="border border-slate-300 text-center text-xl font-bold">
-                    <MdOutlineArrowDropDown />
+
+                  {/* left side Option */}
+                  <td
+                    onClick={() => handleCellClickLeft(item?.num)}
+                    className="border border-slate-300 text-center p-[3px] cursor-pointer"
+                    key={item.num}
+                  >
+                    {openIndexLeft === item?.num ? (
+                      <div className="relative border-4 border-red-800  flex items-center justify-center">
+                        <div
+                          className={`absolute  ${
+                            openIndexLeft === 25 ||
+                            openIndexLeft === 26 ||
+                            openIndexLeft === 27 ||
+                            openIndexLeft === 28 ||
+                            openIndexLeft === 29 ||
+                            openIndexLeft === 30 ||
+                            openIndexLeft === 31
+                              ? " left-[70px] -rotate-90"
+                              : "-top-4"
+                          } bg-slate-100 border border-purple-700 w-9 text-black text-center`}
+                        >
+                          {/* Close button */}
+
+                          {numberStore ? (
+                            <p className="text-lg mx-auto font-bold text-gray-200 bg-purple-800 w-full">
+                              {numberStore}
+                            </p>
+                          ) : (
+                            <IoMdArrowDropup className="text-2xl mx-auto font-bold text-gray-200 bg-purple-800 w-full" />
+                          )}
+                          {Array.from({ length: 7 }).map((_, index) => (
+                            <p
+                              key={index}
+                              onClick={() => setNumberStore(index + 1)}
+                            >
+                              {index + 1}
+                            </p>
+                          ))}
+                        </div>
+                      </div>
+                    ) : (
+                      <MdOutlineArrowDropDown className="text-2xl font-bold text-gray-500" />
+                    )}
                   </td>
                 </tr>
               ))}
